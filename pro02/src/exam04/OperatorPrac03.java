83bf4c1279d8d3afd887ae5ac7a48f615c5fc187
이보슬 <tool0923@naver.com>
2022-03-08 11:18:53 +0900

    .

--- pro02/src/exam04/OperatorPrac03.java
+++ pro02/src/exam04/OperatorPrac03.java
@@ -9,7 +9,7 @@ public class OperatorPrac03 {
 		// 입력 받은 값에 대해 산술 연산, 비교연산을 수행하도록 만든다
 		
 		/*
-		 * 출력 형식
+		 * 출력 형식  
 		 * 
 		 * 첫 번째 값 입력 : 10
 		 * 두 번째 값 입력 : 15
@@ -21,6 +21,7 @@ public class OperatorPrac03 {
 		 * 나누기 계산 결과는 실수값으로 나오게 한다. 그리고 반드시 소수점 4번째 자리까지만 나오게 한다.
 		 */
 		
+		//내가 짠 코드
 		Scanner sc = new Scanner(System.in);
 		System.out.print("첫 번째 값 입력 : ");
 		int num1 = sc.nextInt();
@@ -33,27 +34,27 @@ public class OperatorPrac03 {
 		System.out.printf("나머지 계산 결과 : %d\n", num1 % num2);
 		
 		//다른 방법
-		int n1, n2, r1, r2, r3, r5;
+		int n1, n2, r1, r2, r3, r5;                    // 선언부
 		double r4;
 		
-		System.out.print("첫 번째 값 입력 : ");
+		System.out.print("첫 번째 값 입력 : ");           // 초기화
 		n1 = sc.nextInt();
 		System.out.print("두 번째 값 입력 : ");
 		n2 = sc.nextInt();
 		
-		r1 = n1 + n2;
+		r1 = n1 + n2;                                 // 로직 / 연산
 		r2 = n1 - n2;
 		r3 = n1 * n2;
 		r4 = (double)n1 / n2;
 		r5 = n1 % n2;
 		
-		System.out.println("더하기 계산 결과 : " + r1);
+		System.out.println("더하기 계산 결과 : " + r1);   // 출력
 		System.out.println("빼기 계산 결과 : " + r2);
 		System.out.println("곱하기 계산 결과 : " + r3);
 		System.out.printf("나누기 계산 결과 : %.4f\n" + r4);
 		System.out.println("나머지 계산 결과 : " + r5);
 		
-		
+		// 선언부 - 초기화 - 로직/연산 - 출력
 		
 		
 	}
