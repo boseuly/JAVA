ef0d8f127dd9407c22c10680ff9c0625c03e054c
이보슬 <tool0923@naver.com>
2022-03-15 21:29:36 +0900

    카이스르 암호 만들기

--- pro04/src/exam01/Array05.java
+++ pro04/src/exam01/Array05.java
@@ -20,12 +20,15 @@ public class Array05 {
 		System.out.print("영단어를 입력하시오 : ");
 		String sInput= sc.nextLine();
 		
-		//암호화가 되기 
-		char origin[] = new char[sInput.length()];   //메서드 : 이건 문자열의 길이 구하는 것    //사용자가 입력한 문자열의 크기만큼 문자배열 만들어라
+		//방법1) 암호화
+		char origin[] = new char[sInput.length()];   //메서드 : 이건 문자열의 길이 구하는 것  //사용자가 입력한 문자열의 크기만큼 문자배열 만들어라
 		
 		for(int i = 0; i < origin.length ; i++) {
-			origin[i] = sInput.charAt(i);
+			origin[i] = sInput.charAt(i);			//문자열의 i번째 문자를 char로 변환해서 저장
 		}
+		
+		//방법2) .tocharArray 메서드를 사용하여 문자열을 문자배열로 변환
+		origin = sInput.toCharArray();	
 		
 		char[] crypto = new char[origin.length];   //배열 길이 속성 : 이건 배열의 길이 구하는 것
 		for(int i = 0; i <crypto.length; i++) {
@@ -38,10 +41,20 @@ public class Array05 {
 		
 		String res1 = "";
 		String res2 = "";
+		// 방법1) 문자 배열을 문자열로 만들어서 출력하기 위해 반복문을 사용
 		for(int i = 0; i <origin.length; i++) {
 			res1 += origin[i];
 			res2 += crypto[i];
 		}
+		// 방법2) new 연산으로 문자열 생성을 할 때 문자 배열을 넣는다.
+		res1 = new String(origin);
+		res2 = new String(crypto);
+		
+		// 방법3) String.vauleOf(문자배열)메서드를 사용한다.
+		res1 = String.valueOf(origin);
+		res2 = String.valueOf(crypto);
+		// 세가지 방법 중 편한대로 
+		
 		System.out.println("암호화 전 : " + res1);
 		System.out.println("암호화 후 : " +  res2);
 		
