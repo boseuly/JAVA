8ee9b852e96e9c390f87735aea7bdafd86e86962
이보슬 <tool0923@naver.com>
2022-03-17 15:14:28 +0900

    직접 다이어그램 만들어 보기

---
+++ pro05/src/exam06/StopWatch.java
@@ -0,0 +1,25 @@
+package exam06;
+
+public class StopWatch {
+/*	
+	+------------------------+	
+	|	exam06.StopWatch     |		
+	+------------------------+	
+	| + seconds : int 		 |
+	| + minutes : int 		 |
+	| + hours	: int  		 |
+	+------------------------+	
+	| + start	: 시작		 |	
+	| + pause	: 일시정지		 |
+	| + stop 	: 멈춤		 |
+    | + 		: 리셋		 |
+	+------------------------+	
+	
+		
+		
+*/		
+		
+		
+		
+
+}
