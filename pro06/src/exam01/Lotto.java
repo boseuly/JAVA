13b8186199de0d510440bd5f02db9ece16e574a9
이보슬 <tool0923@naver.com>
2022-03-22 23:28:05 +0900

    Lotto 미완성

--- pro06/src/exam01/Lotto.java
+++ pro06/src/exam01/Lotto.java
@@ -43,14 +43,42 @@ public class Lotto {
 	public int[] numbers() {
 		return numbers;
 	}
-	//메소드
+	//메소드	//중복 검사도 해야 됨
 	public void generate() {
 		numbers = new int[count];
+		int temp[] = new int[count];	//중복검사할 때 필요함
+		
 		Random random = new Random();
-		for(int i = 0; i < numbers.length; i++) {
+		numbers[0] = random.nextInt(Max-1)+Min;
+		temp[0] = numbers[0];
+		
+		for(int i = 1; i < numbers.length; i++) {
 			numbers[i] = random.nextInt(Max-1)+Min;
+			for(int j = 0; j < i-1; j++) {
+				if(numbers[i] == temp[j]) {
+					
+					break;		//만약 같다면?? 
+				}else {
+					temp[j] = numbers[i];
+				}
+			}
+			
+			
 		}
-		System.out.println(Arrays.toString(numbers));
+		
 	}
+	//매개변수로 들어간 수가 꼭 로또 번호로 들어가 있어야 한다. 
+	public void generate(int n1) {
+		numbers = new int[count-1];
+		Random random = new Random();
+		for(int  i = 0; i < numbers.length; i++) {
+			numbers[i] = random.nextInt(Max-1) + Min;
+		}
+		int temp[] = new int[numbers.length + 1 ];
+		System.arraycopy(numbers, 0, temp, 0, numbers.length);
+		numbers = temp;
+		
+	}
+	
 	
 }
