069e15b659a81455132f9b44955b60cbf93621fb
이보슬 <tool0923@naver.com>
2022-03-23 16:57:17 +0900

    ReportGrade 첫 번째 지문 어떻게 코드짜는지 감이 잘 안 잡힘

--- pro06/src/exam01/Run.java
+++ pro06/src/exam01/Run.java
@@ -20,7 +20,11 @@ public class Run {
 		System.out.println(g.getName());
 		System.out.println(g.getScore());
 		System.out.println(g.getRank());
+		System.out.println("------구분선-----");
+		Grade g1 = new Grade("영어", 90.6);
+		System.out.println(g1.getRank());
 		
+		System.out.println("------구분선-----");
 		Lotto lot = new Lotto();
 		lot.generate();
 		System.out.println(Arrays.toString(lot.getNumbers()));
@@ -31,19 +35,37 @@ public class Run {
 		lot.generate(10,20,30);
 		System.out.println(Arrays.toString(lot.getNumbers()));
 	
-		
-		
-		
-		ReportGrade rg = new ReportGrade("이보슬");
-		System.out.println(rg.getName());
-		
-		String[] str = new String[] {"국어", "영어"};
-		rg.setSubjects(str);
+		System.out.println("------구분선-----");
+			
+		/*
+		 * - 점수를 설정 할 때에는 어떤 과목에 대한 점수인지를 알 수 있도록 setter의
+		 *   매개변수를 조정하여 사용하도록 한다.
+		 * 
+		 * - 과목 배열의 인덱스와 점수 배열의 인덱스는 서로 동일한 인덱스간에 
+		 *   연계되는 형태이다. 국어 과목의 인덱스가 0번이면 해당 과목의 점수는 0번에
+		 *   저장되어 있는 방식이다.
+		 * 
+		 * - 위에 제시한 멤버변수 및 메서드 외에 추가로 필요한 멤버변수 및 메서드가
+		 *   있는 경우 추가해도 됨
+		 * 
+		 */
+		String[] str = new String[] {"국어", "영어", "수학"};
+		ReportGrade rg = new ReportGrade("이보슬", str);
 		System.out.println(Arrays.toString(rg.getSubjects()));
-		
-		double[] grades = new double[]{70.7, 46.8, 89};
-		rg.setGrades(grades);
+		rg.setGrades(90.0, 80.0, 95.4);
 		System.out.println(Arrays.toString(rg.getGrades()));
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 		
 	}
--- pro06/src/exam02/ReportGrade.java
+++ pro06/src/exam02/ReportGrade.java
@@ -1,14 +1,13 @@
 package exam02;
 
 public class ReportGrade {
-	private String name;
-	private String subjects[];
-	private double grades[];
+	private String name;		//학생명
+	private String subjects[];	//과목명이 저장되는 멤버변수
+	private double grades[];	//점수가 저장되는 멤버변수
 	
 	//매개변수 있는 생성자
 	public ReportGrade(String name) {
 		this.name = name;
-		
 	}
 	public ReportGrade(String name, String[] subjects) {
 		this.name = name;
@@ -28,8 +27,10 @@ public class ReportGrade {
 	public String[] getSubjects() {
 		return subjects;
 	}
-	public void setSubjects(String[] subjects) {	
-		this.subjects = subjects;
+	public void setSubjects(String ...strings) {	
+		for(int i = 0; i < strings.length; i++) {
+			this.subjects[i] = strings[i];
+		}
 	}
 	
 	
@@ -37,9 +38,9 @@ public class ReportGrade {
 	public double[] getGrades() {
 		return grades;
 	}
-	public void setGrades(double[] grades) {
-		this.grades = grades;
-		
+	public void setGrades(double ...ds) {	//점수를 가변인자로 받기
+		this.grades = ds;
 	}
+	
 	
 }
