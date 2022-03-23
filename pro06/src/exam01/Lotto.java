6ee14892910449ea0f269d2b0caefe976e10c209
이보슬 <tool0923@naver.com>
2022-03-23 14:02:25 +0900

    Lotto(중복검사는 아직 안 함)

--- pro06/src/exam01/Lotto.java
+++ pro06/src/exam01/Lotto.java
@@ -4,34 +4,37 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class Lotto {
-	private int Min = 1;
-	private int Max = 45;
-	private int count = 6;
+	private int rangeMin = 1;
+	private int rangeMax = 45;
+	private int count = 6;	//numbers배열의 크기
 	private int numbers[];
-	
+	private Random random = new Random();	//반복해서 쓰이니까 멤버변수로 만들어둠
 	//기본생성자
-	public Lotto() {}
+	public Lotto() {
+		this.numbers = new int[count];		//이런 설정은 생성자에 넣어두기 
+	}
 	//매개변수 있는 생성자
 	public Lotto(int min, int max, int count) {
-		this.Min = min;
-		this.Max = max;
+		this.rangeMin = min;
+		this.rangeMax = max;
 		this.count = count;
-	}
+		this.numbers = new int[count];		//여기서는 this() 할 수 없음 -> this()는 위에 써야 하는데 그렇게 되면 배열의 크기가 초기값인 6으로 생성되기 때문에
+	}										//그 다음에 매개변수로 입력을 받음 -> 배열의 크기는 변할 수 없음
 	//getter & setter
-	public int getMin() {
-		return Min;
+	public int getRangeMin() {
+		return rangeMin;
 	}
 	
-	public void setMin(int min) {
-		this.Min = min;
+	public void setRangeMin(int min) {
+		this.rangeMin = min;
 	}
 	
-	public int getMax() {
-		return Max;
+	public int getRangeMax() {
+		return rangeMax;
 	}
 	
-	public void setMax(int max) {
-		this.Max = max;
+	public void setRangeMax(int max) {
+		this.rangeMax = max;
 	}
 	
 	public int getCount() {
@@ -40,45 +43,37 @@ public class Lotto {
 	public void setCount(int count) {
 		this.count = count;
 	}
-	public int[] numbers() {
+	public int[] getNumbers() {
 		return numbers;
 	}
 	//메소드	//중복 검사도 해야 됨
-	public void generate() {
-		numbers = new int[count];
-		int temp[] = new int[count];	//중복검사할 때 필요함
+	public void generate(int ...nums) {		//nums는 배열임-> nums[0], nums[1]	... 인스턴스 생성할 때 매개변수(nums)의 값을 넣어줌
+		int cnt = nums.length;				//cnt = nums의 길이
 		
-		Random random = new Random();
-		numbers[0] = random.nextInt(Max-1)+Min;
-		temp[0] = numbers[0];
-		
-		for(int i = 1; i < numbers.length; i++) {
-			numbers[i] = random.nextInt(Max-1)+Min;
-			for(int j = 0; j < i-1; j++) {
-				if(numbers[i] == temp[j]) {
-					
-					break;		//만약 같다면?? 
-				}else {
-					temp[j] = numbers[i];
-				}
-			}
-			
-			
+		for(int  i = 0; i < cnt; i++) {		//들어온 매개변수 값(numbers배열의 값)을 
+			numbers[i] = nums[i];			//numbers[i]에 넣어줘야 한다.
 		}
 		
-	}
-	//매개변수로 들어간 수가 꼭 로또 번호로 들어가 있어야 한다. 
-	public void generate(int n1) {
-		numbers = new int[count-1];
-		Random random = new Random();
-		for(int  i = 0; i < numbers.length; i++) {
-			numbers[i] = random.nextInt(Max-1) + Min;
+		for(int i = cnt; i < count; i++) {	//nums값을 numbers 배열에 넣었응까 나머지는 random으로 이어서 넣어주기
+			numbers[i] = this.random.nextInt(rangeMax) + rangeMin;
 		}
-		int temp[] = new int[numbers.length + 1 ];
-		System.arraycopy(numbers, 0, temp, 0, numbers.length);
-		numbers = temp;
+//		int temp[] = new int[count];	//중복검사할 때 필요함
+//		Random random = new Random();
+//		numbers[0] = random.nextInt(rangeMax-1)+rangeMin;
+//		temp[0] = numbers[0];
+//		
+//		for(int i = 1; i < numbers.length; i++) {
+//			numbers[i] = random.nextInt(rangeMax-1)+rangeMin;
+//			for(int j = 0; j < i-1; j++) {
+//				if(numbers[i] == temp[j]) {
+//					
+//					break;		//만약 같다면?? 
+//				}else {
+//					temp[j] = numbers[i];
+//				}
+//			}
+//		}
 		
 	}
-	
 	
 }
--- pro06/src/exam01/Run.java
+++ pro06/src/exam01/Run.java
@@ -1,15 +1,44 @@
 package exam01;
 
+import java.util.Arrays;
+
+import exam02.ReportGrade;
+
 public class Run {
 
 	public static void main(String[] args) {
-		Lotto l = new Lotto();
-		l.generate();
-		System.out.println("-----수정후-----");
-		l.setMin(4);
-		l.setMax(67);
-		l.setCount(8);
-		l.generate();
+//		Lotto l = new Lotto();
+//		l.generate();
+//		System.out.println("-----수정후-----");
+//		l.setMin(4);
+//		l.setMax(67);
+//		l.setCount(8);
+//		l.generate();
+//		
+		Grade g = new Grade("국어");
+		g.setScore(80.4);
+		System.out.println(g.getName());
+		System.out.println(g.getScore());
+		System.out.println(g.getRank());
+		
+		Lotto lot = new Lotto();
+		lot.generate();
+		System.out.println(Arrays.toString(lot.getNumbers()));
+		
+		lot.generate(1);
+		System.out.println(Arrays.toString(lot.getNumbers()));
+		
+		lot.generate(10,20,30);
+		System.out.println(Arrays.toString(lot.getNumbers()));
+	
+		ReportGrade rg = new ReportGrade("이보슬");
+		System.out.println(rg.getName());
+		String[] str = new String[] {"국어", "영어"};
+		rg.setSubjects(str);
+		System.out.println(Arrays.toString(rg.getSubjects()));
+		double[] grades = new double[]{70.7, 46.8, 89};
+		rg.setGrades(grades);
+		System.out.println(Arrays.toString(rg.getGrades()));
 	}
-
+	
 }
