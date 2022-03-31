057c0901a0c34cc8e88bbf5c9e016eb9bd3f7ecf
이보슬 <tool0923@naver.com>
2022-03-31 10:29:59 +0900

    3/31 Interface

---
+++ pro07/src/exam09/Shape.java
@@ -0,0 +1,9 @@
+package exam09;
+
+public interface Shape {
+	
+	public abstract double getArea();	// abstract를 써도 되고 
+	public double getRount();			// 안 써도 됨. abstract가 생략되어 있다.
+	
+	
+}
---
+++ pro07/src/exam09/Square.java
@@ -0,0 +1,15 @@
+package exam09;
+
+public class Square implements Shape {
+
+	@Override
+	public double getArea() {
+		return 0;
+	}
+
+	@Override
+	public double getRount() {
+		return 0;
+	}
+
+}
